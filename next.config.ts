0a3f27c1292e9ad84b4813ba3dd5a62c3dd423cd
John Mark Delima <delimajohnmark14@gmail.com>
2024-12-31 19:03:24 +0800

    Update Next.js image configuration to support remote patterns for TMDB images

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ['image.tmdb.org']
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
