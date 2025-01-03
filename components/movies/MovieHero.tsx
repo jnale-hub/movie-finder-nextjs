57ce242454f4b6756c649faebfa2d7cc41c58153
John Mark Delima <delimajohnmark14@gmail.com>
2025-01-03 17:22:32 +0800

    Refactor MovieHero component: improve button structure and enhance video overlay styling

--- components/movies/MovieHero.tsx
+++ components/movies/MovieHero.tsx
@@ -20,54 +20,58 @@ export function MovieHero({ movie }: MovieHeroProps) {
     <div className="w-2/3 relative rounded-md overflow-hidden">
       {movie.videos?.results && movie.videos.results.length > 0 && (
         <>
+          <div className="absolute size-full bg-gradient-to-t from-neutral-950 opacity-75"></div>
+          <Image
+            src={`https://img.youtube.com/vi/${movie.videos.results[0].key}/maxresdefault.jpg`}
+            alt={`${movie.Title} Thumbnail`}
+            className="size-full object-cover"
+            width={200}
+            height={100}
+          />
 
-<div className="absolute size-full bg-gradient-to-t from-neutral-950 opacity-50"></div>
-            <Image
-              src={`https://img.youtube.com/vi/${movie.videos.results[0].key}/maxresdefault.jpg`}
-              alt={`${movie.Title} Thumbnail`}
-              className="size-full object-cover"
-              width={200}
-              height={100}
-            />
-
-            <button
+          <button
+            onClick={() => setIsPlaying(true)}
+            className="absolute bottom-4 left-4 flex gap-4 items-center group transform"
+          >
+            <div className="max-md:hidden flex lg:size-16 size-12 items-center justify-center rounded-full bg-transparent lg:border-4 border-[3px] border-white group-hover:border-amber-400">
+              <FaPlay className="lg:size-6 size-4 pl-1 group-hover:text-amber-400" />
+            </div>
+            <span
               onClick={() => setIsPlaying(true)}
-              className="absolute bottom-4 left-4 flex gap-4 items-center group transform"
+              className="text-sm md:text-xl"
             >
-              <div className="max-md:hidden flex lg:size-16 size-12 items-center justify-center rounded-full bg-transparent lg:border-4 border-[3px] border-white group-hover:border-amber-400">
-                <FaPlay className="lg:size-6 size-4 pl-1 group-hover:text-amber-400" />
-              </div>
-              <span onClick={() => setIsPlaying(true)} className="text-sm md:text-xl">Play trailer</span>
-            </button>
+              Play trailer
+            </span>
+          </button>
 
-            <button
-              onClick={() => setIsPlaying(true)}
-              className="md:hidden absolute group left-1/2 max-md:top-1/2 max-md:-translate-x-1/2 max-md:-translate-y-1/2 transform"
-            >
-              <div className="flex  size-12 items-center justify-center rounded-full bg-transparent border-[3px] border-white group-hover:border-amber-400">
-                <FaPlay className="md:size-6 size-4 pl-1 group-hover:text-amber-400" />
-              </div>
-            </button>
+          <button
+            onClick={() => setIsPlaying(true)}
+            className="md:hidden absolute group left-1/2 max-md:top-1/2 max-md:-translate-x-1/2 max-md:-translate-y-1/2 transform"
+          >
+            <div className="flex  size-12 items-center justify-center rounded-full bg-transparent border-[3px] border-white group-hover:border-amber-400">
+              <FaPlay className="md:size-6 size-4 pl-1 group-hover:text-amber-400" />
+            </div>
+          </button>
 
-            {isPlaying && (
-              <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
-                <div className="relative aspect-video w-11/12 max-w-4xl">
-                  <button
-                    onClick={() => setIsPlaying(false)}
-                    className="absolute -top-10 right-0 text-white"
-                  >
-                    Close
-                  </button>
-                  <iframe
-                    src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?autoplay=1`}
-                    title={`${movie.Title} Trailer`}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                    className="h-full w-full"
-                  />
-                </div>
+          {isPlaying && (
+            <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+              <div className="relative aspect-video w-11/12 max-w-4xl">
+                <button
+                  onClick={() => setIsPlaying(false)}
+                  className="absolute -top-10 right-0 text-white"
+                >
+                  Close
+                </button>
+                <iframe
+                  src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?autoplay=1`}
+                  title={`${movie.Title} Trailer`}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="h-full w-full"
+                />
               </div>
-            )}
+            </div>
+          )}
         </>
       )}
     </div>
