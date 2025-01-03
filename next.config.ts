07cda10401c7661a3924d5a4efb252a85266bba8
John Mark Delima <delimajohnmark14@gmail.com>
2025-01-03 16:56:18 +0800

    Add YouTube image domain to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
+    domains: ['img.youtube.com'], 
     remotePatterns: [
       {
         protocol: 'https',
